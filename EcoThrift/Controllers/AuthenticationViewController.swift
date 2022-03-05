d8cee7e0cca25a7232bfbeb914b24aa3113cc4ff
Alex <alex37888@gmail.com>
2022-03-05 15:05:03 -0800

    Added firebase oath

---
+++ EcoThrift/Controllers/AuthenticationViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AuthenticationViewController.swift
+//  EcoThrift
+//
+//  Created by Alex Roman on 3/5/22.
+//
+
+import UIKit
+
+class AuthenticationViewController: UIViewController {
+
+    @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+    
+
+   
+
+}
---
+++ EcoThrift/Controllers/LoginViewController.swift
@@ -0,0 +1,40 @@
+//
+//  LoginViewController.swift
+//  EcoThrift
+//
+//  Created by Alex Roman on 3/5/22.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButtone: UIButton!
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func onLogin(_ sender: UIButton){
+        Auth.auth().signIn(withEmail: emailTextField.text ?? "" , password: passwordTextField.text ?? "") { Auth, error in
+            if let error = error as NSError? {
+                print(error.localizedDescription)
+            } else {
+                print("Logged in")
+                //segue
+            }
+        }
+    }
+    
+    
+
+}
