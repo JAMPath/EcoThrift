d7af57035629f867996918c08884938fda8f6f39
Alex <alex37888@gmail.com>
2022-03-05 05:48:09 -0800

    Created landing screen

---
+++ EcoThrift/Controllers/AuthenticationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AuthenticationViewController.swift
+//  EcoThrift
+//
+//  Created by Alex Roman on 3/5/22.
+//
+
+import UIKit
+
+class AuthenticationViewController: UIViewController {
+
+    @IBOutlet weak var signupButton: UIButton!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
