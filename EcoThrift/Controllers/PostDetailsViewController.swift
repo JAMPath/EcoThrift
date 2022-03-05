1075bfefe79891526c76ac0a61bd0e47fdf6dd09
Moises Limon <moises.m.limon@gmail.com>
2022-03-05 08:35:57 -0800

    Implemented initial structure for Profile View Controller and the additional Post Details View Controller that follows it.

---
+++ EcoThrift/Controllers/PostDetailsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  PostDetailsViewController.swift
+//  EcoThrift
+//
+//  Created by Moises Limon on 3/5/22.
+//
+
+import UIKit
+
+class PostDetailsViewController: UIViewController {
+
+    
+    @IBOutlet weak var location: UILabel!
+    //
+    @IBOutlet weak var postName: UILabel!
+    @IBOutlet weak var postImage: UIImageView!
+    @IBOutlet weak var postPrice: UILabel!
+    //
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ EcoThrift/Storyboards/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  EcoThrift
+//
+//  Created by Moises Limon on 3/5/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
