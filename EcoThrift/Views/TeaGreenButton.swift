4d49214326b119e8d99930a74f0636d5423086c8
Alex <alex37888@gmail.com>
2022-03-05 08:42:35 -0800

    Added teaButton & Firebase

---
+++ EcoThrift/Views/TeaGreenButton.swift
@@ -0,0 +1,20 @@
+//
+//  TeaGreenButton.swift
+//  EcoThrift
+//
+//  Created by Alex Roman on 3/5/22.
+//
+
+import UIKit
+
+class TeaGreenButton: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
