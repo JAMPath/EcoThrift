35093e41644fceb797ecfe618ec1f5298784cafd
Alex <alex37888@gmail.com>
2022-03-06 02:52:46 -0800

    Added post products Api

--- EcoThrift/Controllers/AuthenticationViewController.swift
+++ EcoThrift/Controllers/AuthenticationViewController.swift
@@ -30,36 +30,39 @@ class AuthenticationViewController: UIViewController {
         //let size = 5
         
         
-        for i in 1...5{
-            let id = Int.random(in: 1...99999)
-            let cost = Double.random(in: 1.0...50.0)
-            self.ref.child("Products").child("Product_\(id)").setValue("3")
-            self.ref.child("Products").child("Product_\(id)").child("Name").setValue("Item-\(id)")
-            self.ref.child("Products").child("Product_\(id)").child("Price").setValue("\(cost)")
-            self.ref.child("Products").child("Product_\(id)").child("ImgURL").setValue("https://aws.atomretro.com/xlarge/madcap-england-shockwave-knitted-polo-sorrel-brown-3.jpg")
-        }
+//        for i in 1...5{
+//            let id = Int.random(in: 1...99999)
+//            let cost = Double.random(in: 1.0...50.0)
+//            self.ref.child("Products").child("Product_\(id)").setValue("3")
+//            self.ref.child("Products").child("Product_\(id)").child("Name").setValue("Item-\(id)")
+//            self.ref.child("Products").child("Product_\(id)").child("Price").setValue("\(cost)")
+//            self.ref.child("Products").child("Product_\(id)").child("ImgURL").setValue("https://aws.atomretro.com/xlarge/madcap-england-shockwave-knitted-polo-sorrel-brown-3.jpg")
+//        }
+//
+//        self.ref.child("Thriftstores").setValue("3")
+//
+//        for i in 1...3 {
+//            let id = Int.random(in: 1...9999)
+//            let lat = Double.random(in: 32.20...32.40)
+//            let long = Double.random(in:(-120.50)...(-120.35))
+//                    self.ref.child("Thriftstores").child("Thriftstore_\(id)").setValue("4")
+//            self.ref.child("Thriftstores").child("Thriftstore_\(id)").child("lat").setValue("\(lat)")
+//            self.ref.child("Thriftstores").child("Thriftstore_\(id)").child("long").setValue("\(long)")
+//            self.ref.child("Thriftstores").child("Thriftstore_\(id)").child("ImgURL").setValue("https://www1.cbn.com/sites/default/files/styles/video_ratio_16_9/public/media/slider/images/salvationarmy_hdv.jpg?itok=ObmqRCjs")
+//            self.ref.child("Thriftstores").child("Thriftstore_\(id)").child("phone-number").setValue("2097778888")
+//        }
         
-        self.ref.child("Thriftstores").setValue("3")
-        
-        for i in 1...3 {
-            let id = Int.random(in: 1...9999)
-            let lat = Double.random(in: 32.20...32.40)
-            let long = Double.random(in:(-120.50)...(-120.35))
-                    self.ref.child("Thriftstores").child("Thriftstore_\(id)").setValue("4")
-            self.ref.child("Thriftstores").child("Thriftstore_\(id)").child("lat").setValue("\(lat)")
-            self.ref.child("Thriftstores").child("Thriftstore_\(id)").child("long").setValue("\(long)")
-            self.ref.child("Thriftstores").child("Thriftstore_\(id)").child("ImgURL").setValue("https://www1.cbn.com/sites/default/files/styles/video_ratio_16_9/public/media/slider/images/salvationarmy_hdv.jpg?itok=ObmqRCjs")
-            self.ref.child("Thriftstores").child("Thriftstore_\(id)").child("phone-number").setValue("2097778888")
-        }
-        
+        self.ref.child("Owners").setValue("10")
         for i in 1...10 {
             let id = Int.random(in: 1...9999)
             let followers = Int.random(in: 1...999)
             let following = Int.random(in: 1...100)
             let sales     = Float.random(in: 10.0...100000.0)
             
-            self.ref.child("Owners").child("Owner#\(id)").setValue(10)
-            self.ref.child("Owners").child("Owner#\(id)").child("followers").setValue("\(followers)")
+            self.ref.child("Owners").child("Owner_\(id)").setValue(10)
+            self.ref.child("Owners").child("Owner_\(id)").child("Followers").setValue("\(followers)")
+            self.ref.child("Owners").child("Owner_\(id)").child("Following").setValue("\(following)")
+            self.ref.child("Owners").child("Owner_\(id)").child("Sales").setValue("\(sales)")
         }
         
         
@@ -71,16 +74,16 @@ class AuthenticationViewController: UIViewController {
 
     @IBAction func onSave(_ sender: Any) {
         i += 1
-        
-        let API = DatabaseWrapper()
-        API.getAllProducts { key, value in
-            print("\(key):\(value)")
-        }
-        
-        API.getAllStores { key, value in
-            print("\(key):\(value)")
-        }
-        
+        //addProduct()
+//        let API = DatabaseWrapper()
+//        API.getAllProducts { key, value in
+//            print("\(key):\(value)")
+//        }
+//
+//        API.getAllStores { key, value in
+//            print("\(key):\(value)")
+//        }
+//
         
         
 //        self.ref.child("Products").setValue("Product-\(i)")
--- EcoThrift/Controllers/DatabaseWrapper.swift
+++ EcoThrift/Controllers/DatabaseWrapper.swift
@@ -8,17 +8,23 @@
 import Foundation
 import Firebase
 import FirebaseDatabase
+import FirebaseStorage
+import UIKit
 
 
 class DatabaseWrapper{
     var baseRef: DatabaseReference!
     var products: [String:Any]
     var handlers: [() -> Void]
+    var storage:Storage!
+    var storageRef:StorageReference!
     
     init(){
         baseRef = Database.database().reference()
         products = [:]
         handlers = []
+        storage  = Storage.storage()
+        storageRef = storage.reference()
     }
     
     
@@ -45,6 +51,65 @@ class DatabaseWrapper{
         }
     }
     
+    func getAllOwners(completionHandler: @escaping (_ key:Any, _ value: Any)->Void){
+        baseRef.child("Owners").observeSingleEvent(of: .value) { snapshot in
+            if let dict = snapshot.value as? Dictionary<String, Dictionary<String, Any>> {
+                for (key, value) in dict {
+                    completionHandler(key,value)
+                }
+            }
+        }
+    }
+    
+    func postProduct(name:String, price:Double, size:Character, image: UIImage) throws {
+        let id = Int.random(in: 1...9999)
+        let sizes = ["s","m","l"]
+        baseRef.child("Products").child("Product_\(id)").setValue("3")
+        baseRef.child("Products").child("Product_\(id)").child("Name").setValue(name)
+        baseRef.child("Products").child("Product_\(id)").child("Price").setValue(price)
+        baseRef.child("Products").child("Product_\(id)").child("Size").setValue(sizes[Int.random(in: 0...2)])
+        
+        let urlPath = try uploadImage(to: "images/products", image: image)
+
+        if urlPath == nil{
+            throw DatabaseError.serverError
+        }
+        baseRef.child("Products").child("Product_\(id)").child("ImgURL").setValue(urlPath)
+        
+    }
+    
+    private func uploadImage(to storagePath:String, image:UIImage) throws ->URL?{
+        let imageData = image.pngData()
+        let locationRef = storageRef.child(storagePath)
+        
+        if imageData != nil{
+            throw DatabaseError.clientError
+    
+        }
+        
+        
+        
+        var urlPath:URL?
+        let uploadTask = locationRef.putData(imageData!, metadata: nil) { (metadata, error) in
+            guard let metadata = metadata else {
+                print("Unable to upload")
+                return
+            }
+            let size = metadata.size
+            
+            locationRef.downloadURL { url, error in
+                guard let downloadURL = url else {
+                    return
+                }
+                
+                urlPath = url
+            }
+        }
+        
+        return urlPath
+        
+    }
+    
 }
 
 
@@ -65,7 +130,7 @@ extension DatabaseError: LocalizedError {
         case .droppedRequest:
             return NSLocalizedString("", comment: "")
         case .clientError:
-            return NSLocalizedString("", comment: "")
+            return NSLocalizedString("Encountered an error while preparing to upload resources", comment: "")
         }
     }
 }
