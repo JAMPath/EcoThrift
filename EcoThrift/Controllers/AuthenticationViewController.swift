55ddb743778c4133d488d9a8ffd5dd8c54f7f55a
Alex <alex37888@gmail.com>
2022-03-05 23:38:53 -0800

    filled database

--- EcoThrift/Controllers/AuthenticationViewController.swift
+++ EcoThrift/Controllers/AuthenticationViewController.swift
@@ -21,19 +21,39 @@ class AuthenticationViewController: UIViewController {
         super.viewDidLoad()
         ref = Database.database().reference()
         i = 1
-        self.ref.child("Products").setValue("Product-\(1)")
+        
     }
     
-    
+
     
     func addProduct(){
-        let size = 5
-        self.ref.child("Products").setValue(5)
+        //let size = 5
+        
         
         for i in 1...5{
             let id = Int.random(in: 1...99999)
-            self.ref.child("Products").child("Product_\(id)").setValue("")
+            let cost = Double.random(in: 1.0...50.0)
+            self.ref.child("Products").child("Product_\(id)").setValue("3")
+            self.ref.child("Products").child("Product_\(id)").child("Name").setValue("Item-\(id)")
+            self.ref.child("Products").child("Product_\(id)").child("Price").setValue("\(cost)")
+            self.ref.child("Products").child("Product_\(id)").child("ImgURL").setValue("https://aws.atomretro.com/xlarge/madcap-england-shockwave-knitted-polo-sorrel-brown-3.jpg")
         }
+        
+        self.ref.child("Thriftstores").setValue("3")
+        
+        for i in 1...3 {
+            let id = Int.random(in: 1...9999)
+            let lat = Double.random(in: 32.20...32.40)
+            let long = Double.random(in:(-120.50)...(-120.35))
+                    self.ref.child("Thriftstores").child("Thriftstore_\(id)").setValue("4")
+            self.ref.child("Thriftstores").child("Thriftstore_\(id)").child("lat").setValue("\(lat)")
+            self.ref.child("Thriftstores").child("Thriftstore_\(id)").child("long").setValue("\(long)")
+            self.ref.child("Thriftstores").child("Thriftstore_\(id)").child("ImgURL").setValue("https://www1.cbn.com/sites/default/files/styles/video_ratio_16_9/public/media/slider/images/salvationarmy_hdv.jpg?itok=ObmqRCjs")
+            self.ref.child("Thriftstores").child("Thriftstore_\(id)").child("phone-number").setValue("2097778888")
+        }
+        
+        
+        
                     
         
     }
